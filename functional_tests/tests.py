56a675130428031ee5c8b3c250d0da48b8c480e5
Omah <omah@ithuan.tw>
2022-08-11 09:46:38 +0800

    new URL + view for adding to existing lists. FT passes :-)

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -112,7 +112,6 @@ class NewVisitorTest(LiveServerTestCase):
         # Edith wonders whether the site will remember her list. Then she sees
         # that the site has generated a unique URL for her -- there is some
         # explanatory text to that effect.
-        self.fail('Finish the test!')
 
 
 if __name__ == '__main__':
